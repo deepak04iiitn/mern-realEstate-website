4def07b5e2ad8f3582d6777c80d5dbe459d39f8a
deepak04iiitn <dky422003@gmail.com>
2024-06-09 14:07:22 +0530

    completed create listing page functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,6 +1,6 @@
 import Listing from "../models/listing.model.js";
 
-export const createListing = async (res , req , next) => {
+export const createListing = async (req , res , next) => {
     try {
         
         const listing = await Listing.create(req.body);
