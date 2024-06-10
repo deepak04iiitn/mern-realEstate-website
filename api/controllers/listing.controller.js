5016b94db1a6b2dc56feed7f2a9260811df4e083
deepak04iiitn <dky422003@gmail.com>
2024-06-11 01:22:17 +0530

    completed edit user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -39,7 +39,7 @@ export const deleteListing = async (req, res, next) => {
     if (!listing) {
       return next(errorHandler(404, 'Listing not found!'));
     }
-    
+
     if (req.user.id !== listing.userRef) {
       return next(errorHandler(401, 'You can only update your own listings!'));
     }
@@ -51,6 +51,22 @@ export const deleteListing = async (req, res, next) => {
         { new: true }
       );
       res.status(200).json(updatedListing);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const getListing = async (req, res, next) => {
+    try {
+        
+      const listing = await Listing.findById(req.params.id);
+
+      if (!listing) {
+        return next(errorHandler(404, 'Listing not found!'));
+      }
+
+      res.status(200).json(listing);
+
     } catch (error) {
       next(error);
     }
