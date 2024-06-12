dfd878e75eb3aa224b0c871cb09ddd16bc179875
deepak04iiitn <dky422003@gmail.com>
2024-06-12 12:29:04 +0530

    added pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -12,9 +13,12 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to MongoDB");
 }).catch((err) => {
     console.log(err);
-})
+});
+
+const __dirname = path.resolve(); 
 
 const app = express();
+
 app.use(express.json());
 
 app.use(cookieParser());
@@ -26,6 +30,13 @@ app.listen(3000 , () => {
 app.use('/api/user' , userRouter);                               
 app.use('/api/auth' , authRouter); 
 app.use('/api/listing' , listingRouter); 
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  })
 
 
 app.use((err , req , res , next) => {           /* we use next to go to the next middleware */
